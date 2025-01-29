746aebc06fd943b97af1140051c9795cfcbdc405
luisSteel07 <luisdelfonseca220@gmail.com>
2025-01-28 23:15:04 -0500

    Init

---
+++ TODO List/Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+public class Persona
+{
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Crear un objeto
+        Persona persona = new Persona { Nombre = "Juan", Edad = 30 };
+
+        // Serializar el objeto a JSON
+        string json = JsonConvert.SerializeObject(persona);
+        Console.WriteLine("JSON: " + json);
+
+        // Deserializar el JSON a un objeto
+        Persona personaDeserializada = JsonConvert.DeserializeObject<Persona>(json);
+        Console.WriteLine("Nombre: " + personaDeserializada.Nombre);
+        Console.WriteLine("Edad: " + personaDeserializada.Edad);
+    }
+}
